af0aefd42ea85f4a0f13f047e6f7e5f671c0a39f
Isabel <74679950+isabelMNT@users.noreply.github.com>
2020-11-26 21:06:59 -0800

    add sketch from week 1

---
+++ week1_1/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(800, 800);
+  
+
+}
+
+function draw() {
+  var size;                                //introduction
+  size = random (0,50) + 10;               //assignment
+  fill(280,0, frameCount % 120) +5;
+ ellipse(mouseX, mouseY, size,size);
+  
+  var temp = 99.5;                        //number
+  var year = 2020;
+  var lotto1 =round(random(0,50));
+  var lotto2 =round(random(0,50));
+  
+  var name = "Isabel";
+  var lastname = 'Rodriguez';
+
+  
+  var isIsItSunny = true;               // boolean
+  var isRainy = false;
+  
+  var isRepublican = null; //we dont know (yet)
+  temp = null; 
+  
+ 
+}
