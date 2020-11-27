9791757db3d746a9dd4675f1bf0e080f00115e00
Isabel <74679950+isabelMNT@users.noreply.github.com>
2020-11-26 22:52:41 -0800

    update sketch week 2_2

--- week2_2/sketch.js
+++ week2_2/sketch.js
@@ -1,56 +1,72 @@
-var angle = 0;
-var centerX = 200;
-var centerY = 200;
+var x = [];
+var y = [];
+var speed = [];
+var angle;
+let scoreElem;
 
 
 
 
 function setup() {
   createCanvas(800, 800);
-  
+  angle = PI / 4;
+
 
 }
 
 function draw() {
-  
-     if (mouseY < 400) {
-    fill('gray');  
-  
-  } else {            
-    fill('pink');   
-  }
-  
-  
-  
-  
-  if (mouseIsPressed) { 
-  
-  if (mouseX < 400) {
-    
- 
-  var size;                                
-  size = random (2,50) + 2;    
- 
-  stroke(100, 100, 240);
-          fill(100,5, frameCount % 20) +100;
-   rect(mouseX, mouseY, size,size);
-    
-    
+  //background('black');
 
-        } else {                           
- 
+  for (var i = 1; i < x.length; i = i + 1) {
 
-  
-  
-  var size;                                
-  size = random (0,100) + 5;
-          noFill();
-  stroke(5,0, frameCount % 300) +5;
-        
- ellipse(mouseX, mouseY, size,size);
-          
-          
-    
-        }
+    translate(width / 2, height / 2);
+    rotate(angle);
+    scale(-1, 1);
+    translate(-width / 2, -height / 2);
+    stroke('black')
+    strokeWeight(1);
+
+    var size;
+    size = random(0, 50);
+    blendMode(SOFT_LIGHT);
+    fill(random(255), random(255), random(255));
+    ellipse(x[i], y[i], size, size);
+
+    var size;
+    size = random(0, 100);
+    noFill();
+    stroke(5, 0, frameCount % 300) + 5;
+   ellipse(x[i], y[i], size, size);
+
+
+   
+
+
+
+
+
+      var dx = mouseX - x[i];
+      var dy = mouseY - y[i];
+
+      var dist = abs(dx) + abs(dy);
+
+      if (dist > 0) {
+        //rect(x, y, 20)
+        dx = dx / dist * speed[i];
+        dy = dy / dist * speed[i];
+      }
+
+      x[i] = x[i] + dx;
+      y[i] = y[i] + dy;
+    }
   }
+
+  function mouseClicked() {
+    print("mouseClicked frame " + frameCount);
+    x.push(mouseX);
+    y.push(mouseY);
+    speed.push(random(10, 10));
+
   }
+
+
